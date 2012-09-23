cde893c7f15e216c5ba2f6528a411066c0018412
Navaneeth <nramana@thoughtworks.com>
2012-09-23 06:57:41 +0530

    #2UI | customize controls, price range in accom' page | <Navaneeth>

--- lets-share/js/script.js
+++ lets-share/js/script.js
@@ -26,3 +26,24 @@ var userOperation = {
 	logout: function(){
 	}
 }
+
+$('[data-role="page"]').live('pageshow', function () {
+    //priceRange
+    $('#buying_slider_min').change(function() {
+        var min = parseInt($(this).val());
+        var max = parseInt($('#buying_slider_max').val());
+        if (min > max) {
+            $(this).val(max);
+            $(this).slider('refresh');
+        }
+    });
+    $('#buying_slider_max').change(function() {
+        var min = parseInt($('#buying_slider_min').val());
+        var max = parseInt($(this).val());
+
+        if (min > max) {
+            $(this).val(min);
+            $(this).slider('refresh');
+        }
+    });
+});
